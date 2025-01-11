6d685f4788c98cdb44331f664e4470c174556ec7
edwardspot <edwardspotgaming@gmail.com>
2025-01-10 22:10:25 -0800

    knightattack

---
+++ knightattack.js
@@ -0,0 +1,16 @@
+class KnightAttack {
+	constructor(game) {
+		this.game = game;
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./Attack2.png"), 0, 0, 125, 100, 6, 0.1);
+
+	};
+
+	update() {
+
+	};
+
+	draw(ctx) {
+		this.animator.drawFrame(this.game.clockTick, ctx, 25, 25);
+		//ctx.drawImage(ASSET_MANAGER.getAsset("./Attack3.png"),0,0);
+	};
+};
